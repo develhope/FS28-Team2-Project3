0b38db18fdf6cf39e2018599edaf36529cb48ed6
mario-sica/Mario <mario97sica@gmail.com>
2024-07-05 13:35:26 +0200

    Feat: Added Icon functionality/latest style version

--- JS/createCard.js
+++ JS/createCard.js
@@ -59,8 +59,6 @@ async function fetchData() {
   try {
       const response = await fetch('https://raw.githubusercontent.com/mario-sica/API/main/db.json');
       const data = await response.json();
-      console.log(data); // Ora i dati sono disponibili nella variabile 'data'
-      // Puoi usare 'data' come desideri, ad esempio assegnarlo a un'altra variabile
       let jsonData = data;
 
       function renderListings(filteredListing, maxCards = filteredListing.length) {
@@ -88,12 +86,11 @@ async function fetchData() {
                 <div class="swiper-button-prev"></div>
             </div>
             <div class="card-content">
-                <h2 class="location">${listing.location}</h2>
-                <p class="host">Host:${listing.host}<br>01-07 lug</p>
-                <div class="rating-price">
-                  <span class="rating">${listing.rating}</span>
-                  <span class="price">${listing.price}</span>
-                </div>
+                <div class="location fs-15">${listing.location}</div>
+                <div class="host fs-15">Host:${listing.host}</div>
+                <div class="available fs-15">18-28 set</div>
+                <div class="price fs-15">${listing.price} €<span> notte</span></div>
+                <div class="rating fs-15"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 12px; width: 12px; fill: currentcolor;"><path fill-rule="evenodd" d="m15.1 1.58-4.13 8.88-9.86 1.27a1 1 0 0 0-.54 1.74l7.3 6.57-1.97 9.85a1 1 0 0 0 1.48 1.06l8.62-5 8.63 5a1 1 0 0 0 1.48-1.06l-1.97-9.85 7.3-6.57a1 1 0 0 0-.55-1.73l-9.86-1.28-4.12-8.88a1 1 0 0 0-1.82 0z"></path></svg>${listing.rating}</div>
             </div>
           `;
       
@@ -110,16 +107,18 @@ async function fetchData() {
     }
     
     // Gestione del clic sulle icone per filtrare le strutture
-    document.querySelectorAll('.icon').forEach(icon => {
-      icon.addEventListener('click', () => {
-          const category = icon.getAttribute('data-category');
-          const filteredListing = strutture.filter(listing => listing.category === category);
-          renderListings(filteredListing);
+    document.querySelectorAll('.place-choice').forEach(place => {
+      place.addEventListener('click', () => {
+          const category = place.id;
+          const word = category.split(" ")
+          const filteredListing = jsonData.filter(el => el.category.includes(word));
+
+          renderListings(filteredListing, 24);
       });
     });
     
     // Visualizza tutte le strutture all'avvio della pagina
-    renderListings(jsonData, 10);
+    renderListings(jsonData, 24);
 
   } catch (error) {
       console.error('Errore:', error);
--- JS/scroll.js
+++ JS/scroll.js
@@ -1,18 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
   const searchbar = document.querySelector('.search-bar');
+  const miniSearch = document.getElementById('mini-search')
   const links = document.querySelector('.middle-links');
-  const navbar = document.querySelector('.nav-container')
-  const threshold = 1;
+  const navbar = document.querySelector('.nav-container');
+  const veil = document.getElementById('overlay')
+  const threshold = 0;
 
-  window.addEventListener('scroll', () => {
+  function handleScroll() {
     if (window.scrollY > threshold) {
       links.classList.add('links-scroll');
       searchbar.classList.add('search-scroll');
       navbar.classList.add('scrolled');
+      miniSearch.classList.add('reveal');
+      veil.classList.remove('veil');
+
     } else {
       links.classList.remove('links-scroll');
       searchbar.classList.remove('search-scroll');
       navbar.classList.remove('scrolled');
+      miniSearch.classList.remove('reveal');
     }
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
+  miniSearch.addEventListener('click', () => {
+    window.removeEventListener('scroll', handleScroll);
+
+    links.classList.remove('links-scroll');
+    searchbar.classList.remove('search-scroll');
+    navbar.classList.remove('scrolled');
+    miniSearch.classList.remove('reveal');
+    veil.classList.toggle('veil');
+
+    setTimeout(() => {
+      window.addEventListener('scroll', handleScroll);
+    }, 100);
   });
+  
+  veil.addEventListener('click', () => {
+    veil.classList.remove('veil');
+    links.classList.add('links-scroll');
+    searchbar.classList.add('search-scroll');
+    navbar.classList.add('scrolled');
+    miniSearch.classList.add('reveal');
+
+  })
 });
