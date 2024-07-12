5c0f9bf354bcd5cd4d002b1d8fccc67b93f75396
Luigi Cavalli <luigi_cavalli@outlook.com>
2024-07-12 12:40:20 +0200

    fix: minor fix

--- JS/hidden-navbar.js
+++ JS/hidden-navbar.js
@@ -1,10 +1,23 @@
-document.addEventListener('scroll', function () {
-  const threshold = 640;
-  const hiddenNavbar = document.querySelector('.hidden-navbar');
+document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('scroll', function () {
+    const threshold = 640;
+    const hiddenNavbar = document.querySelector('.hidden-navbar');
 
-  if (window.scrollY > threshold) {
-    hiddenNavbar.classList.add('visible');
-  } else {
-    hiddenNavbar.classList.remove('visible');
-  }
+    if (window.scrollY > threshold) {
+      hiddenNavbar.classList.add('visible');
+    } else {
+      hiddenNavbar.classList.remove('visible');
+    }
+  });
+
+  document.addEventListener('scroll', function () {
+    const threshold = 1790;
+    const afterCalendar = document.querySelector('.show-after-calendar');
+
+    if (window.scrollY > threshold) {
+      afterCalendar.classList.add('visible');
+    } else {
+      afterCalendar.classList.remove('visible');
+    }
+  });
 });
